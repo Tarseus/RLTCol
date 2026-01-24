3448e78a7259a1c66f3ab2d7ab39707951df5829
gsd_g52 <gsd2003_20@163.com>
2026-01-24 04:50:57 +0000

    update test script

--- test.sh
+++ test.sh
@@ -4,25 +4,28 @@
 #   --sa-converge --sa-steps 50000 --sa-stall-steps 2000 --sa-alpha 0.995 \
 #   --episodes 1 --num-envs 1 --sa-log-interval 0 --log-interval 1 > test_tsp100.log
 
+export CUDA_VISIBLE_DEVICES=5
+
+# python src/routing_runner.py \
+#   --problem tsp --mode rlho --nodes 50 --policy outputs/tsp50.pt \
+#   --rl-steps 128 --sa-t0 5 \
+#   --sa-converge --sa-steps 50000000 --sa-alpha 0.9999995993977071 \
+#   --episodes 1000 --num-envs 256 --sa-log-interval 100000 --sa-log-env 0
+
 python src/routing_runner.py \
   --problem tsp --mode rlho --nodes 100 --policy outputs/tsp100.pt \
   --rl-steps 128 --sa-t0 5 \
-  --sa-converge --sa-steps 50000 --sa-stall-steps 2000 \
-  --episodes 1 --num-envs 1 --sa-log-interval 500 --sa-log-env 0
+  --sa-converge --sa-steps 50000000 --sa-alpha 0.9999995993977071 \
+  --episodes 1000 --num-envs 256 --sa-log-interval 100000 --sa-log-env 0
 
-# python src/routing_runner.py \
-#   --problem tsp --mode rlho --nodes 100 --policy outputs/tsp100.pt \
-#   --rl-steps 128 --sa-t0 5 \
-#   --sa-converge --sa-steps 50000 --sa-stall-steps 2000 \
-#   --episodes 1000 --num-envs 16 --log-interval 10 \
-#   --sa-log-interval 500
+python src/routing_runner.py \
+  --problem cvrp --mode rlho --customers 100 --capacity 50 --policy outputs/cvrp100.pt \
+  --rl-steps 128 --sa-t0 5 \
+  --sa-converge --sa-steps 50000000 --sa-alpha 0.9999995993977071 \
+  --episodes 1000 --num-envs 256 --sa-log-interval 100000 --sa-log-env 0
 
-# python src/routing_runner.py \ 0.9999938
-#   --problem cvrp --mode rlho --nodes 100 --policy outputs/cvrp100.pt \
-#   --rl-steps 128 --sa-t0 5 \
-#   --sa-converge --sa-steps 50000 --sa-stall-steps 2000
-
-# python src/routing_runner.py \
-#   --problem cvrp --mode rlho --nodes 50 --policy outputs/cvrp50.pt \
-#   --rl-steps 128 --sa-t0 5 \
-#   --sa-converge --sa-steps 50000 --sa-stall-steps 2000
+python src/routing_runner.py \
+  --problem cvrp --mode rlho --customers 50 --capacity 40 --policy outputs/cvrp50.pt \
+  --rl-steps 128 --sa-t0 5 \
+  --sa-converge --sa-steps 50000000 --sa-alpha 0.9999995993977071 \
+  --episodes 1000 --num-envs 256 --sa-log-interval 100000 --sa-log-env 0
